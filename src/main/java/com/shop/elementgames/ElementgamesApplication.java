1bc95a2b357a282899be93bd8dc8cd14ca3dbe63
Manan Walia <mananwalia959@gmail.com>
2021-08-05 12:22:38 +0530

    feat: initial commit

---
+++ src/main/java/com/shop/elementgames/ElementgamesApplication.java
@@ -0,0 +1,13 @@
+package com.shop.elementgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/elementgames/ElementgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.elementgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
