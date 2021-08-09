31f5152ce512efccb9b3856a2d1ddaa65b704383
Manan Walia <mananwalia959@gmail.com>
2021-08-09 16:19:18 +0530

    initial routing setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , pathMatch: 'full' , component: HomeComponent },
+  { path: 'product/:id' , component: ProductComponent } ,
+  { path : '**' , redirectTo : ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
