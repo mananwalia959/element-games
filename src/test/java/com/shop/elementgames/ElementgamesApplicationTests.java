9845e776249c240b8e346d7f981441c221228a07
Manan Walia <mananwalia959@gmail.com>
2021-08-15 11:09:38 +0530

    Revert "feat: add static to gitignore temporarily"
    This reverts commit 93df454e35afbad1f79a01bb4fa7b0622cd199f2.

--- src/test/java/com/shop/elementgames/ElementgamesApplicationTests.java
+++ src/test/java/com/shop/elementgames/ElementgamesApplicationTests.java
@@ -1,20 +1,13 @@
 package com.shop.elementgames;
 
-import java.util.regex.Pattern;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ElementgamesApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
-		var result = Pattern.matches("[^\\.]*", "index.html");
-		System.out.println(result);
 	}
-	
-	
 
 }
