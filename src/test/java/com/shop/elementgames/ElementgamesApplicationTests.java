93df454e35afbad1f79a01bb4fa7b0622cd199f2
Manan Walia <mananwalia959@gmail.com>
2021-08-15 11:09:04 +0530

    feat: add static to gitignore temporarily

--- src/test/java/com/shop/elementgames/ElementgamesApplicationTests.java
+++ src/test/java/com/shop/elementgames/ElementgamesApplicationTests.java
@@ -1,13 +1,20 @@
 package com.shop.elementgames;
 
+import java.util.regex.Pattern;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ElementgamesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		var result = Pattern.matches("[^\\.]*", "index.html");
+		System.out.println(result);
 	}
+	
+	
 
 }
