5b22ea076a807cf0543fc5e7f923990d2c26d192
Manan Walia <mananwalia959@gmail.com>
2021-08-19 20:01:25 +0530

    fix : nested paths redirect to spa

--- src/main/java/com/shop/elementgames/controller/SpaController.java
+++ src/main/java/com/shop/elementgames/controller/SpaController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaController {
 	// redirects all paths except containind dots or leftover from other contollers
-	// https://stackoverflow.com/a/48622614.
-	@RequestMapping(value =  {"/{path:[^\\.]*}" })
+	// https://stackoverflow.com/a/56187917
+	@RequestMapping(value =  "/**/{path:[^\\.]*}")
 	public String forward(HttpServletRequest request) {
 		return "forward:/index.html";
 	}
