c0455611c3cf0997ff81bb91f5a3399d0d726b25
Manan Walia <mananwalia959@gmail.com>
2021-08-28 12:43:32 +0530

    feat: add bootstrap for responsive styling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 
@@ -28,7 +30,7 @@ import {MatButtonModule} from '@angular/material/button';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
