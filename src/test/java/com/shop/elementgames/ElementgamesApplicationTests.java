2cb2f0783f9b28e9609b9bd0423bdcf3e54f2a86
Manan Walia <mananwalia959@gmail.com>
2021-09-05 16:55:22 +0530

    ci : fix test case

--- src/test/java/com/shop/elementgames/ElementgamesApplicationTests.java
+++ src/test/java/com/shop/elementgames/ElementgamesApplicationTests.java
@@ -2,8 +2,11 @@ package com.shop.elementgames;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:applcation-test.properties")
 class ElementgamesApplicationTests {
 
 	@Test
